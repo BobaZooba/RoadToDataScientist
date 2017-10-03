cbabcbc8b9d70689b0fcab7d3880f2bfd99e9605
Boris Zubarev <bobazooba@gmail.com>
2017-10-04 01:18:28 +0500

    up

--- contests/OTP_bank/data/OTP Chaid.R
+++ contests/OTP_bank/data/OTP Chaid.R
@@ -1,8 +1,6 @@
-install.packages("CHAID", repos="http://R-Forge.R-project.org")
-
 set.seed(42)
 library(CHAID)
 
 
-data <- read.csv("data/Credit_OTP.csv")
+data <- read.csv("contests/OTP_bank/data/Credit_OTP.csv")
 
